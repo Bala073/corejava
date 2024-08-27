eaa5e7c91da95eafccce30557b9b0969941de441
Bala073 <balakishanpotla21666@gmail.com>
2024-08-27 18:59:51 +0530

    Add files via upload

---
+++ Developement/generalprogramming/Biggest_of_three_numbers.java
@@ -0,0 +1,24 @@
+package com.evergent.corejava.generalprogramming;
+
+public class Biggest_of_three_numbers {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b=20;
+		int c=30;
+		if(a>b&&a>c)
+		{
+			System.out.println("a is greater");
+		}
+		else if(b>a&&b>c)
+		{
+			System.out.println("b is greater");
+		}
+		else
+		{
+			System.out.println("c is greater");
+		}
+
+	}
+
+}
