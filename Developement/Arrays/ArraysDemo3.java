b94d884206236b0d441fd758b5154f211324caea
Bala073 <balakishanpotla21666@gmail.com>
2024-08-27 11:20:41 +0530

    Add files via upload

---
+++ Developement/Arrays/ArraysDemo3.java
@@ -0,0 +1,18 @@
+package com.evergent.corejava.arrays;
+
+public class ArraysDemo3 {
+
+	public static void main(String[] args) {
+		int arr[]=new int[5];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=i;
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
